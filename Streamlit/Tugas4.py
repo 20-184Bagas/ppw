65cdbea6be74a427fedadeb65ad1ff545880b981
Bagas Pratama Putra <125527857+20-184Bagas@users.noreply.github.com>
2023-12-12 15:03:38 +0700

    Update Tugas4.py

--- Streamlit/Tugas4.py
+++ Streamlit/Tugas4.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import nltk
+
+# Unduh data NLTK (jalankan sekali untuk mengunduh)
+nltk.download('stopwords')
+nltk.download('punkt')
 
 # Fungsi untuk membersihkan teks
 def clean_text(text):
